d9e88b137069f1bc42fafd6760f2c2cef6db9896
Luong La <85066482+luong11026@users.noreply.github.com>
2024-03-04 18:16:19 +0700

    feat: enhanced get acitivities api (#4)
    * create api doc
    
    * feat: enhanced get activity API with precondition
    
    * feat: add query with status API
    
    * resolve conflict
    
    * resolve conflict
    
    * remove .idea
    
    ---------
    
    Co-authored-by: Hai Bui <haibui.cs@gmail.com>
    Co-authored-by: haibt1112 <84982551+haibt1112@users.noreply.github.com>

--- backend/src/main/java/com/dalv/bksims/controllers/social_points_management/ActivityController.java
+++ backend/src/main/java/com/dalv/bksims/controllers/social_points_management/ActivityController.java
@@ -6,7 +6,13 @@ import com.dalv.bksims.services.social_points_management.ActivityService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import net.kaczmarzyk.spring.data.jpa.domain.GreaterThan;
+import net.kaczmarzyk.spring.data.jpa.domain.LessThanOrEqual;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Or;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -16,8 +22,8 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @Tag(name = "Activity")
 @RestController
@@ -44,10 +50,38 @@ public class ActivityController {
     @Secured({"ROLE_STUDENT", "ROLE_LECTURER", "ROLE_ADMIN"})
     public ResponseEntity<Page<Activity>> findActivityWithPagination(
             @PathVariable int offset,
-            @PathVariable int pageSize
+            @PathVariable int pageSize,
+            @RequestParam(value = "order", required = false, defaultValue = "ASC") String order,
+            @RequestParam(value = "status", required = false, defaultValue = "ALL") String status,
+            @Or({
+                    @Spec(path = "location", params = "query", spec = Like.class),
+                    @Spec(path = "title", params = "query", spec = Like.class)
+            })
+            Specification<Activity> activitySpec,
+            @Spec(path = "endDate", constVal = "#{T(java.time.LocalDate).now()}", valueInSpEL = true, spec = LessThanOrEqual.class)
+            Specification<Activity> AcitivityWithClosedStatus,
+            @Spec(path = "endDate", constVal = "#{T(java.time.LocalDate).now()}", valueInSpEL = true, spec = GreaterThan.class)
+            Specification<Activity> AcitivityWithOpenStatus
+
     ) {
-        Page<Activity> activitiesWithPagination = activityService.findActivityWithPagination(offset, pageSize);
-        return new ResponseEntity<>(activitiesWithPagination, HttpStatus.OK);
+        return switch (status) {
+            case "OPEN" -> {
+                Page<Activity> activitiesWithPaginationOpen = activityService.findActivityWithPagination(
+                        activitySpec.and(AcitivityWithOpenStatus), offset, pageSize, order);
+                yield new ResponseEntity<>(activitiesWithPaginationOpen, HttpStatus.OK);
+            }
+            case "CLOSED" -> {
+                Page<Activity> activitiesWithPaginationClosed = activityService.findActivityWithPagination(
+                        activitySpec.and(AcitivityWithClosedStatus), offset, pageSize, order);
+                yield new ResponseEntity<>(activitiesWithPaginationClosed, HttpStatus.OK);
+            }
+            default -> {
+                Page<Activity> activitiesWithPagination = activityService.findActivityWithPagination(activitySpec,
+                                                                                                     offset, pageSize,
+                                                                                                     order);
+                yield new ResponseEntity<>(activitiesWithPagination, HttpStatus.OK);
+            }
+        };
     }
 
     @PatchMapping("/{title}")
