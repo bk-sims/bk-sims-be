98294966c934bf1cb431553d81ce440a9d01197f
Hai Bui <haibui.cs@gmail.com>
2024-03-18 00:20:22 +0700

    add find all participants by activity title api

--- backend/src/main/java/com/dalv/bksims/models/repositories/social_points_management/ActivityParticipationRepository.java
+++ backend/src/main/java/com/dalv/bksims/models/repositories/social_points_management/ActivityParticipationRepository.java
@@ -1,9 +1,8 @@
 package com.dalv.bksims.models.repositories.social_points_management;
 
-import com.dalv.bksims.models.entities.social_points_management.Activity;
 import com.dalv.bksims.models.entities.social_points_management.ActivityParticipation;
 import com.dalv.bksims.models.entities.social_points_management.ActivityParticipationId;
-import org.springframework.data.domain.Page;
+import com.dalv.bksims.models.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +13,7 @@ import java.util.UUID;
 public interface ActivityParticipationRepository extends JpaRepository<ActivityParticipation, ActivityParticipationId> {
     @Query("SELECT ap.activity.id FROM ActivityParticipation ap WHERE ap.user.id = :userId")
     List<UUID> findActivityIdByUserId(@Param("userId") UUID userId);
+
+    @Query("SELECT ap.user FROM ActivityParticipation ap WHERE ap.activity.title = :activityTitle")
+    List<User> findUserByActivityTitle(@Param("activityTitle") String activityTitle);
 }
