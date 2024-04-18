1cf22b71b48b108d6dc349a0c6e18542ffd6ee76
luongla <luong11026org@gmail.com>
2024-04-18 21:43:06 +0700

    enable cors

--- backend/src/main/java/com/dalv/bksims/configurations/AppConfiguration.java
+++ backend/src/main/java/com/dalv/bksims/configurations/AppConfiguration.java
@@ -29,7 +29,7 @@ public class AppConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
