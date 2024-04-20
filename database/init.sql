342e15920c90fa682c339e47ab86f1d54864f0ee
luongla <luong11026org@gmail.com>
2024-04-21 01:52:50 +0700

    fix: fix CD pipeline

--- database/init.sql
+++ database/init.sql
@@ -118,6 +118,27 @@ CREATE TABLE token (
     CONSTRAINT fk_token_user FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
 
+CREATE TABLE course (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    course_code VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    department VARCHAR(255) UNIQUE NOT NULL,
+    credits INT NOT NULL,
+    introduction VARCHAR(255) NOT NULL,
+    syllabus_file_name VARCHAR(255),
+    exercise INT NOT NULL,
+    midterm INT NOT NULL,
+    assignment INT NOT NULL,
+    final INT NOT NULL
+);
+
+CREATE TABLE prerequisites (
+    pre_course_id UUID NOT NULL,
+    course_id UUID NOT NULL,
+    CONSTRAINT fk_pre_course_id FOREIGN KEY (pre_course_id) REFERENCES course (id),
+    CONSTRAINT fk_participation_user FOREIGN KEY (course_id) REFERENCES course (id),
+    PRIMARY KEY (pre_course_id, course_id)
+);
 
 -- Insert initial values for tables
 
@@ -296,3 +317,84 @@ INSERT INTO student (id, user_id, program_id, department_id) VALUES
     (SELECT id FROM program WHERE name = 'General Program'),
     (SELECT id FROM department WHERE name = 'Civil Engineering')
 );
+
+-- Inserting courses into the course table
+-- Test syllabus https://bk-sims-storage.s3.ap-northeast-2.amazonaws.com/syllabus/mock_syllabus.pdf
+-- Mon Dai Cuong
+INSERT INTO course (id, course_code, name, credits, introduction, syllabus_file_name, exercise, midterm, assignment, final)
+VALUES 
+    (uuid_generate_v4(), 'MT1003', 'Calculus 1', 3, 'Calculus course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'MT1005', 'Calculus 2', 2, 'Advanced Calculus course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'MT1007', 'Linear Algebra', 3, 'Linear Algebra course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'PH1003', 'Physics 1', 2, 'Introduction to Physics course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'PH1007', 'Physics 2', 3, 'Advanced Physics course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'CH1003', 'General Chemistry', 2, 'Introduction to Chemistry course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'MT2013', 'Probability and Statistics', 3, 'Probability and Statistics course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'SP1031', 'Philosophy of Marx and Lenin', 3, 'Philosophy course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'SP1033', 'Political Economy of Marx', 2, 'Political Economy course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'SP1035', 'Socialism Science', 3, 'Socialism course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'SP1039', 'History of Vietnamese communist party', 2, 'History of Vietnamese communist party course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'SP1037', 'Ho Chi Minh Ideaology', 3, 'Ho Chi Minh Ideaology', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'SP1007', 'General Law', 2, 'General Law course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'IM1013', 'General Economics', 3, 'Introduction to Economics course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'IM1023', 'Production Management for Engineers', 2, 'Production Management course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'IM1025', 'Project Management for Engineers', 3, 'Project Management course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'IM1027', 'Engineering Economics', 2, 'Engineering Economics course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'IM3001', 'Business Administration for Engineers', 3, 'Business Administration course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'CO1023', 'Digital Systems', 3, 'Digital Systems course', NULL, 20, 25, 25, 30),
+    (uuid_generate_v4(), 'CO1005', 'Introduction to Computing', 2, 'Introduction to Computing course', NULL, 15, 20, 25, 40);
+
+-- Mon co so
+INSERT INTO course (id, course_code, name, credits, introduction, syllabus_file_name, exercise, midterm, assignment, final)
+VALUES
+    (uuid_generate_v4(), 'CO1007', 'Computer Organization and Assembly Language', 3, 'Computer Organization course', NULL, 10, 20, 25, 45),
+    (uuid_generate_v4(), 'CO1027', 'Programming Techniques', 2, 'Programming Techniques course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'CO3093', 'Computer Networking', 3, 'Computer Networking course', NULL, 15, 20, 25, 40),
+    (uuid_generate_v4(), 'CO3001', 'Software Engineering', 3, 'Software Engineering course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'CO2017', 'Operating Systems', 3, 'Operating Systems course', NULL, 15, 20, 30, 35),
+    (uuid_generate_v4(), 'CO2003', 'Data Structures & Algorithms', 2, 'Data Structures & Algorithms course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'CO2011', 'Mathematical Modeling', 3, 'Mathematical Modeling course', NULL, 15, 20, 25, 40),
+    (uuid_generate_v4(), 'CO2007', 'Computer Architecture', 2, 'Computer Architecture course', NULL, 10, 15, 25, 50);
+
+-- Mon tu chon
+INSERT INTO course (id, course_code, name, credits, introduction, syllabus_file_name, exercise, midterm, assignment, final)
+VALUES 
+    (uuid_generate_v4(), 'CO3011', 'Software Project Management', 3, 'Software Project Management course', NULL, 15, 25, 30, 30),
+    (uuid_generate_v4(), 'CO3015', 'Software Testing', 3, 'Software Testing course', NULL, 15, 20, 25, 40),
+    (uuid_generate_v4(), 'CO3017', 'Software Architecture', 3, 'Software Architecture course', NULL, 10, 20, 25, 45),
+    (uuid_generate_v4(), 'CO3021', 'Database Management Systems', 3, 'Database Management Systems course', NULL, 15, 20, 30, 35),
+    (uuid_generate_v4(), 'CO3023', 'Distributed and Object-Oriented Databases', 3, 'Distributed and Object-Oriented Databases course', NULL, 10, 20, 25, 45),
+    (uuid_generate_v4(), 'CO3027', 'E-commerce', 3, 'E-commerce course', NULL, 12, 20, 25, 45),
+    (uuid_generate_v4(), 'CO3029', 'Data Mining', 3, 'Data Mining course', NULL, 15, 20, 30, 35),
+    (uuid_generate_v4(), 'CO3031', 'Algorithm Analysis and Design', 3, 'Algorithm Analysis and Design course', NULL, 20, 20, 30, 30),
+    (uuid_generate_v4(), 'CO3033', 'Information Systems Security', 3, 'Information Systems Security course', NULL, 10, 20, 25, 45),
+    (uuid_generate_v4(), 'CO3037', 'Internet of Things Applications Development', 3, 'Internet of Things Applications Development course', NULL, 10, 20, 25, 45),
+    (uuid_generate_v4(), 'CO3041', 'Intelligent Systems', 3, 'Intelligent Systems course', NULL, 15, 20, 25, 40),
+    (uuid_generate_v4(), 'CO3043', 'Mobile Application Development', 3, 'Mobile Application Development course', NULL, 10, 20, 25, 45),
+    (uuid_generate_v4(), 'CO3045', 'Game Programming', 3, 'Game Programming course', NULL, 15, 20, 25, 40),
+    (uuid_generate_v4(), 'CO3049', 'Web Programming', 3, 'Web Programming course', NULL, 20, 20, 30, 30),
+    (uuid_generate_v4(), 'CO3057', 'Digital Image Processing and Computer Vision', 3, 'Digital Image Processing and Computer Vision course', NULL, 15, 20, 30, 35),
+    (uuid_generate_v4(), 'CO3059', 'Computer Graphics', 3, 'Computer Graphics course', NULL, 20, 20, 30, 30),
+    (uuid_generate_v4(), 'CO3061', 'Introduction to Artificial Intelligence', 3, 'Introduction to Artificial Intelligence course', NULL, 15, 25, 30, 30),
+    (uuid_generate_v4(), 'CO3065', 'Advanced Software Engineering', 3, 'Advanced Software Engineering course', NULL, 20, 20, 30, 30),
+    (uuid_generate_v4(), 'CO3067', 'Parallel Computing', 3, 'Parallel Computing course', NULL, 15, 20, 25, 40),
+    (uuid_generate_v4(), 'CO3069', 'Cryptography and Network Security', 3, 'Cryptography and Network Security course', NULL, 10, 20, 30, 40),
+    (uuid_generate_v4(), 'CO3071', 'Distributed Systems', 3, 'Distributed Systems course', NULL, 15, 20, 30, 35),
+    (uuid_generate_v4(), 'CO3085', 'Natural Language Processing', 3, 'Natural Language Processing course', NULL, 20, 20, 30, 30),
+    (uuid_generate_v4(), 'CO3089', 'Advanced Topics in Computer Science', 3, 'Advanced Topics in Computer Science course', NULL, 15, 25, 30, 30),
+    (uuid_generate_v4(), 'CO3115', 'System Analysis and Design', 3, 'System Analysis and Design course', NULL, 20, 20, 30, 30),
+    (uuid_generate_v4(), 'CO3117', 'Machine Learning', 3, 'Machine Learning course', NULL, 20, 20, 30, 30),
+    (uuid_generate_v4(), 'CO4031', 'Big Data and Decision Support Systems', 3, 'Big Data and Decision Support Systems course', NULL, 15, 20, 30, 35),
+    (uuid_generate_v4(), 'CO4033', 'Big Data Analytics and Business Intelligence', 3, 'Big Data Analytics and Business Intelligence course', NULL, 15, 20, 30, 35),
+    (uuid_generate_v4(), 'CO4035', 'Enterprise Resource Planning Systems', 3, 'Enterprise Resource Planning Systems course', NULL, 20, 20, 30, 30),
+    (uuid_generate_v4(), 'CO4037', 'Management Information Systems', 3, 'Management Information Systems course', NULL, 15, 20, 30, 35);
+
+
+-- Insert prerequisites table
+-- Triet -> KTCT -> CNXHKH -> LSD -> Tu tuong HCM
+INSERT INTO prerequisites (pre_course_id, course_id)
+VALUES
+    ((SELECT id FROM course WHERE course_code = 'SP1031'), (SELECT id FROM course WHERE course_code = 'SP1033')),
+    ((SELECT id FROM course WHERE course_code = 'SP1033'), (SELECT id FROM course WHERE course_code = 'SP1035')),
+    ((SELECT id FROM course WHERE course_code = 'SP1035'), (SELECT id FROM course WHERE course_code = 'SP1039')),
+    ((SELECT id FROM course WHERE course_code = 'SP1039'), (SELECT id FROM course WHERE course_code = 'SP1037'));
