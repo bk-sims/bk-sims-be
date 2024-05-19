5cf9f9e1c3be929a428864dd903fc9903f4cc206
haibt1112 <84982551+haibt1112@users.noreply.github.com>
2024-05-19 23:08:49 +0700

    fix not display swagger (#37)
    Co-authored-by: Hai Bui <hai.bui@zalora.com>

--- backend/src/main/java/com/dalv/bksims/controllers/social_points_management/ActivityController.java
+++ backend/src/main/java/com/dalv/bksims/controllers/social_points_management/ActivityController.java
@@ -27,7 +27,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -37,13 +36,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 @Tag(name = "Activity")
-@Controller
+@RestController
 @RequestMapping("/api/v1/activities")
 @RequiredArgsConstructor
 public class ActivityController {
